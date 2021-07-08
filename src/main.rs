1c0601d4dc8bc04c1c68c8653284c1d544daa21f
Rodolfo Ruiz <rpruiz@me.com>
2021-07-08 11:33:13 -0500

    The simplest rocket app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, World!"
 }
+
+#[get("/hello/<name>")]
+fn hello(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![hello])
+}
+
